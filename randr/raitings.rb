d0cdefaed6554a705e85e1ccebb01dc08a687f4d
Cory Smith <corysmith@csmith-mba.duncllc.com>
2015-09-24 14:07:32 -0500

    updates

--- randr/raitings.rb
+++ randr/raitings.rb
@@ -24,6 +24,15 @@ end
 rateArray.each do |rate|
   email, notes, score = rate.split(',')
   score.to_i
-  session.execute("INSERT INTO raiting (email, score, notes) VALUES ('#{email}', #{score}, '#{notes}')");
+  date = Time.now.to_f
+  session.execute("INSERT INTO raiting (date, email, score, notes) VALUES ('#{date}', '#{email}', #{score}, '#{notes}')");
 end
 
+email = 'corysmith@email.com'
+add = Array.new
+session.execute("SELECT * FROM raiting WHERE email = '#{email}'").each do |row|
+  puts "User #{row['email']} has a score of #{row['score']}."
+  add = row['score']
+end
+
+puts add
