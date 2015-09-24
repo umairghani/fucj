61fd2e15055cde889ef9b7b8bd4d586de486f627
Cory Smith <corysmith@csmith-mba.local>
2015-09-23 22:08:09 -0500

    r&r sql add

--- randr/raitings.rb
+++ randr/raitings.rb
@@ -15,9 +15,43 @@ def new_provider()
 end
 
 def insert_raiting(rate)
-	#sql statement to add raiting from user
+ #sql statement to add raiting from user
+ update = session.prepare (
+  "UPDATE users SET raiting = ? WHERE (email = ?)"
+  )
+
+  stats = [
+    {
+      :raiting => 9,
+      :email => 'blah@blah.com'
+    },
+  ]
+
+  stats.each do |stat|
+    session.execute(update, stat[:raiting], stat[:email])
+
 end
 
 def insert_review(review)
-        #sql statement to add review from user
+  #sql statement to add review from user
+  update = session.prepare (
+  "UPDATE users SET review = ? WHERE (email = ?)"
+  )
+ 
+  stats = [
+    {
+      :review => "so much to say",
+      :email => 'blah@blah.com'
+    },
+  ]
+ 
+  stats.each do |stat|
+    session.execute(update, stat[:review], stat[:email])
+  end
 end
+
+
+
+
+
+
