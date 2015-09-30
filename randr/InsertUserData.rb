9f8d4ed53330846c116c093c8f5071cdd181b93a
Cory Smith <corysmith@cfabbri2-t440.oww.root.lcl>
2015-09-30 14:48:19 -0500

    breaking up the scripts

--- randr/InsertUserData.rb
+++ randr/InsertUserData.rb
@@ -6,16 +6,10 @@ require 'rubygems'
 
 #connect to cassandra default is localhost
 cluster = Cassandra.cluster
-cluster.each_host 
+cluster.each_host
 keyspace = 'fucj'
-session	 = cluster.connect(keyspace)
+session  = cluster.connect(keyspace)
 
-#get test data
-testEmail = 'rschmidt7@sitemeter.com'
-#testEmail = 'bensmith@email.com'
-
-add = Array.new
-usercom = Array.new
 userData = File.read('USER.json')
 userJdata = JSON.parse(userData)
 rateData = File.read('RATE.json')
@@ -29,7 +23,7 @@ def input_new_user (userJdata,session)
      city = line["city"]
      session.execute("INSERT INTO users (firstname, lastname, email, city) VALUES ('#{firstname}', '#{lastname}', '#{email}', '#{city}')");
    end
-end 
+end
 
 def input_rating(rateJdata,session)
    rateJdata.each do |line|
@@ -39,34 +33,10 @@ def input_rating(rateJdata,session)
      notes = line["notes"]
      session.execute("INSERT INTO rating (date, email, score, notes) VALUES ('#{date}', '#{email}', #{score}, '#{notes}')");
    end
-end 
-
-def average_rating(testEmail,session,add) 
-   session.execute("SELECT * FROM rating WHERE email = '#{testEmail}'").each do |row|
-      add.push(row['score'])
-   end
-      sum = 0 
-      total = add.inject{|sum,x| sum + x }
-      average = total / add.length 
-   return(average)
-end 
-
-def user_comments(session,testEmail,usercom)
-   session.execute("SELECT * FROM rating WHERE email = '#{testEmail}'").each do |row|
-      usercom.push(row['notes'])
-   end
-      return(usercom) 
 end
 
-
-input_rating(rateJdata,session)
 input_new_user(userJdata,session)
-myavg = average_rating(testEmail,session,add)
-  puts "Users average score: #{myavg}" 
-usercom = user_comments(session,testEmail,usercom)
-  puts usercom.each { |x| puts x } 
-
-
-
-
+puts "input user done"
+input_rating(rateJdata,session)
+puts "input user done"
 
